6b29bf362e5c33ae2491a080fbcc3ab7e7263346
sheyna <sheynawatkins@gmail.com>
2021-10-13 19:12:08 -0700

    added more code to calculate cookies sold per hour.

--- class-06/demo/cookie-stand/app.js
+++ class-06/demo/cookie-stand/app.js
@@ -4,6 +4,18 @@
 const seattleStore = document.getElementById('seattleList');
 let hours = ['6 a.m.', '7 a.m.', '8 a.m.'];
 
+//  min 6
+// max 15
+// 10 possible values that are between 6 and 15
+// 15 - 6 = 9 + 10
+// math.random() will give us a number between 1 & 10
+// ex.: 2
+// 2 + 6 = 8
+// 1, 2, 3 ,4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// let's say we have a random number of 2.34
+// Math.floor() = 2
+// Math.ceil() = 3
+
 let seattle = {
   name: 'Seattle',
   min: 23,
@@ -15,8 +27,23 @@ let seattle = {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
   getCookieSalesPerHour: function() {
-    // your code goes here
+     // calculate the cookies sold for all 14 hours
+     // write a for loop that uses the length of the hours away in that loop
+
+    // Get the number of customers this hour
+    let customersThisHour = this.getRandomCustomers();
+    let totalCookiesSoldThisHour = Math.ceil(customersThisHour * this.avg);
+    // that value of total cookies sold that hour on to our array
+    this.cookiesSoldEachHourArray.push(totalCookiesSoldThisHour);
+    console.log(this.cookiesSoldEachHourArray);
+
+
+  },
+  renderTheList: function() {
+    this.getCookieSalesPerHour();
+    // code that will render the lists to the page
+    // render the LIs inside our UL
   }
 }
 
-console.log(seattle.getRandomCustomers());
+seattle.renderTheList();
